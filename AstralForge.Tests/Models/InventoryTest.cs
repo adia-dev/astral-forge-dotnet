41393585540d58367e43fbacd4a09258de22d26f
🦀 adia-dev <abdoudu78130@gmail.com>
2024-07-20 15:12:51 +0200

    🧪 Implemented tests for the `VerifyOrder` and `ProduceOrder` methods

--- AstralForge.Tests/Models/InventoryTest.cs
+++ AstralForge.Tests/Models/InventoryTest.cs
@@ -1,10 +1,9 @@
 using System.Collections.Generic;
-using NUnit.Framework;
-using AstralForge.Models;
 using AstralForge.Enums;
-using AstralForge.Factories;
+using AstralForge.Models;
+using NUnit.Framework;
 
-namespace AstralForge.Tests
+namespace AstralForge.Tests.Models
 {
     [TestFixture]
     public class InventoryTests
@@ -111,6 +110,76 @@ namespace AstralForge.Tests
             Assert.That(instructions, Does.Contain("GET_OUT_STOCK 2 Wings_WE1"));
             Assert.That(instructions, Does.Contain("GET_OUT_STOCK 1 Thruster_TE1"));
             Assert.That(instructions, Does.Contain("FINISHED Explorer"));
+        }
+
+        [Test]
+        public void VerifyOrder_ShouldReturnAvailableIfEnoughParts()
+        {
+            // Arrange
+            var inventory = new Inventory();
+            inventory.AddPart(PartType.Hull, "Hull_HE1", 10);
+            inventory.AddPart(PartType.Engine, "Engine_EE1", 10);
+            inventory.AddPart(PartType.Wings, "Wings_WE1", 20);
+            inventory.AddPart(PartType.Thruster, "Thruster_TE1", 10);
+            var order = new Dictionary<string, int> { { "Explorer", 1 } };
+
+            // Act
+            var result = inventory.VerifyOrder(order);
+
+            // Assert
+            Assert.That(result, Is.EqualTo("AVAILABLE"));
+        }
+
+        [Test]
+        public void VerifyOrder_ShouldReturnUnavailableIfNotEnoughParts()
+        {
+            // Arrange
+            var inventory = new Inventory();
+            var order = new Dictionary<string, int> { { "Explorer", 1 } };
+
+            // Act
+            var result = inventory.VerifyOrder(order);
+
+            // Assert
+            Assert.That(result, Is.EqualTo("UNAVAILABLE"));
+        }
+
+        [Test]
+        public void ProduceOrder_ShouldUpdateStockIfOrderFulfilled()
+        {
+            // Arrange
+            var inventory = new Inventory();
+            inventory.AddPart(PartType.Hull, "Hull_HE1", 10);
+            inventory.AddPart(PartType.Engine, "Engine_EE1", 10);
+            inventory.AddPart(PartType.Wings, "Wings_WE1", 20);
+            inventory.AddPart(PartType.Thruster, "Thruster_TE1", 10);
+            var order = new Dictionary<string, int> { { "Explorer", 1 } };
+
+            // Act
+            var result = inventory.ProduceOrder(order);
+
+            // Assert
+            Assert.That(result, Is.EqualTo("STOCK_UPDATED"));
+            var stockReport = inventory.GetStock();
+            Assert.That(stockReport, Does.Contain("Hull - Hull_HE1: 9"));
+            Assert.That(stockReport, Does.Contain("Engine - Engine_EE1: 9"));
+            Assert.That(stockReport, Does.Contain("Wings - Wings_WE1: 18"));
+            Assert.That(stockReport, Does.Contain("Thruster - Thruster_TE1: 9"));
+            Assert.That(stockReport, Does.Contain("Explorer"));
+        }
+
+        [Test]
+        public void ProduceOrder_ShouldReturnErrorIfNotEnoughParts()
+        {
+            // Arrange
+            var inventory = new Inventory();
+            var order = new Dictionary<string, int> { { "Explorer", 1 } };
+
+            // Act
+            var result = inventory.ProduceOrder(order);
+
+            // Assert
+            Assert.That(result, Does.Contain("ERROR Not enough parts to fulfill the order"));
         }
     }
 }
